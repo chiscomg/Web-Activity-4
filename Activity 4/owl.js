e09a46a6c4347d64866330df56d8c3249be213f4
Francisco Monge <chiscomg@gmail.com>
2022-02-22 10:57:51 -0600

    Project Creation

---
+++ Activity 4/owl.js
@@ -0,0 +1,22 @@
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:true
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    })
